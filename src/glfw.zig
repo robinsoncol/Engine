79db7ad86421ef388c48a359106f6654e2115e83
Robinson Collado <robinsoncol@users.noreply.github.com>
2020-02-13 05:14:51 -0500

    Revert changes

--- src/glfw.zig
+++ src/glfw.zig
@@ -8,12 +8,12 @@ pub const GLFW_FALSE = 0;
 pub const GLFWmonitor = @OpaqueType();
 pub const GLFWwindow = @OpaqueType();
 
-pub extern fn glfwInit() u32;
+pub extern fn glfwInit() c_int;
 pub extern fn glfwTerminate() void;
-pub extern fn glfwWindowHint(hint: u32, value: u32) void;
-pub extern fn glfwCreateWindow(width: u32, height: u32, title: ?[*]const u8, monitor: ?*GLFWmonitor, share: ?*GLFWwindow) ?*GLFWwindow;
-pub extern fn glfwGetRequiredInstanceExtensions(count: *u32) [*]const [*]const u8;
+pub extern fn glfwWindowHint(hint: c_int, value: c_int) void;
+pub extern fn glfwCreateWindow(width: c_int, height: c_int, title: ?[*]const u8, monitor: ?*GLFWmonitor, share: ?*GLFWwindow) ?*GLFWwindow;
+pub extern fn glfwGetRequiredInstanceExtensions(count: *c_int) [*]const [*]const u8;
 pub extern fn glfwCreateWindowSurface(instance: v.Instance, window: *GLFWwindow, allocator: ?*const v.AllocationCallbacks, surface: *v.SurfaceKHR) v.Result;
 pub extern fn glfwDestroyWindow(window: ?*GLFWwindow) void;
-pub extern fn glfwWindowShouldClose(window: ?*GLFWwindow) u32;
+pub extern fn glfwWindowShouldClose(window: ?*GLFWwindow) c_int;
 pub extern fn glfwPollEvents() void;
