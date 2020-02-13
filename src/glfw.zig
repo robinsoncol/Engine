a7877cfb2bc72df8ecaa15b1a23e4f0a81e422ee
Robinson Collado <robinsoncol@users.noreply.github.com>
2020-02-13 05:21:44 -0500

    Revert type changes

--- src/glfw.zig
+++ src/glfw.zig
@@ -12,7 +12,7 @@ pub extern fn glfwInit() c_int;
 pub extern fn glfwTerminate() void;
 pub extern fn glfwWindowHint(hint: c_int, value: c_int) void;
 pub extern fn glfwCreateWindow(width: c_int, height: c_int, title: ?[*]const u8, monitor: ?*GLFWmonitor, share: ?*GLFWwindow) ?*GLFWwindow;
-pub extern fn glfwGetRequiredInstanceExtensions(count: *c_int) [*]const [*]const u8;
+pub extern fn glfwGetRequiredInstanceExtensions(count: *u32) [*]const [*]const u8;
 pub extern fn glfwCreateWindowSurface(instance: v.Instance, window: *GLFWwindow, allocator: ?*const v.AllocationCallbacks, surface: *v.SurfaceKHR) v.Result;
 pub extern fn glfwDestroyWindow(window: ?*GLFWwindow) void;
 pub extern fn glfwWindowShouldClose(window: ?*GLFWwindow) c_int;
