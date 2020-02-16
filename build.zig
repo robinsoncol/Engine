31da7452bd63e97e2cf38014012ac8338502ce9b
Robinson Collado <robinsoncol@users.noreply.github.com>
2020-02-15 22:28:43 -0500

    Setup platform folders structure

--- build.zig
+++ build.zig
@@ -11,7 +11,8 @@ pub fn build(b: *Builder) !void {
 
     switch (builtin.os) {
         .windows => {
-            exe.addLibPath("./libs");
+            exe.linkSystemLibrary("./platforms/windows/libs/glfw3");
+            exe.linkSystemLibrary("./platforms/windows/libs/vulkan-1");
             exe.linkSystemLibrary("User32");
             exe.linkSystemLibrary("Gdi32");
             exe.linkSystemLibrary("Shell32");
@@ -20,20 +21,17 @@ pub fn build(b: *Builder) !void {
             @panic("Unsupported OS!");
         }
     }
-
-    exe.linkSystemLibrary("glfw3");
-    exe.linkSystemLibrary("vulkan-1");
     exe.linkSystemLibrary("c");
 
-    const run_cmd = exe.run();
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const runCmd = exe.run();
+    const runStep = b.step("run", "Run the app");
+    runStep.dependOn(&runCmd.step);
 
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
 
     try addShader(b, exe, "shader.vert", "vert.spv");
-    try addShader(b, exe, "shader.frag", "frag.spv");
+    try addShader(b, exe, "shader.frag", "fraglfw.spv");
 }
 
 fn addShader(b: *Builder, exe: var, in_file: []const u8, out_file: []const u8) !void {
@@ -41,15 +39,14 @@ fn addShader(b: *Builder, exe: var, in_file: []const u8, out_file: []const u8) !
     // glslc -o shaders/vert.spv shaders/shader.vert
 
     const dirname = "shaders";
-    const full_in = try path.join(b.allocator, &[_][]const u8{ dirname, in_file });
-    const full_out = try path.join(b.allocator, &[_][]const u8{ dirname, out_file });
+    const fullIn = try path.join(b.allocator, &[_][]const u8{ dirname, in_file });
+    const fullOut = try path.join(b.allocator, &[_][]const u8{ dirname, out_file });
 
-    // Note: glslc comes with Vulkan. Make sure that it's in your system path
-    const run_cmd = b.addSystemCommand(&[_][]const u8{
-        "glslc",
+    const runCmd = b.addSystemCommand(&[_][]const u8{
+        "platforms/windows/tools/glslc.exe",
         "-o",
-        full_out,
-        full_in,
+        fullOut,
+        fullIn,
     });
-    exe.step.dependOn(&run_cmd.step);
+    exe.step.dependOn(&runCmd.step);
 }
